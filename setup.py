cf7426ea5afd5b7848049fe1614495734c990441
Atharva Kulkarni <kulkarniatharva235@gmail.com>
2019-03-28 04:37:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iitbbscliupdates",
-    version="0.0.1",
+    version="0.2",
     author="Atharva Kulkarni",
     author_email="amk11@iitbbs.ac.in",
     description="CLI for IIT Bhubaneswar updates.",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/IronVenom/IIT-Bhubaneswar-CLI',
     packages=setuptools.find_packages(),
-    install_requires  = ['Click','requests','bs4','prettytable','setuptools','lxml'],
+    install_requires  = ['Click','requests','beautifulsoup4','prettytable','setuptools','lxml'],
     entry_points = '''
     [console_scripts]
     iitbbscli  = iitbbscli.app:main
