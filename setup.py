cba86408c4db835ed73f5edef82e55cf6d9be29e
Atharva Kulkarni <38763219+IronVenom@users.noreply.github.com>
2019-04-11 22:14:22 +0530

    Added attendance

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iitbbscliupdates",
-    version="1.0.8",
+    version="1.2",
     author="Atharva Kulkarni",
     author_email="amk11@iitbbs.ac.in",
     license='MIT',
